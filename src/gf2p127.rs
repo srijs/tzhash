2be6c0ea6e0eab03d0c44fe58e922fa1e5de54da
Sam Rijs <srijs@airpost.net>
2015-02-10 15:26:12 +1100

    simple multiplication of gf2p127

--- src/gf2p127.rs
+++ src/gf2p127.rs
@@ -1,45 +1,79 @@
 //#![allow(non_snake_case)]
 
+extern crate std;
 extern crate core;
-extern crate simdty;
 
 use self::core::ops::BitXor;
 
-type Gf2p127 = simdty::i64x2;
+#[unstable(feature = "core")]
+#[simd]
+#[derive(PartialEq, Eq, Copy, Debug)]
+#[repr(C)]
+struct Gf2p127(pub i64, pub i64);
 
-fn zero() -> Gf2p127 {
-    return simdty::i64x2(0, 0);
+#[inline(always)]
+pub fn zero() -> Gf2p127 {
+    return Gf2p127(0, 0);
 }
 
-fn from_int(a: i64) -> Gf2p127 {
-    return simdty::i64x2(0, a);
+#[inline(always)]
+pub fn from_i64(a: i64) -> Gf2p127 {
+    return Gf2p127(0, a);
 }
 
-fn add(a: Gf2p127, b: Gf2p127) -> Gf2p127 {
-    return simdty::i64x2(a.0.bitxor(b.0), a.1.bitxor(b.1));
+#[inline(always)]
+pub fn add(a: Gf2p127, b: Gf2p127) -> Gf2p127 {
+    return Gf2p127(a.0.bitxor(b.0), a.1.bitxor(b.1));
 }
 
-fn mul_00(_: Gf2p127) -> Gf2p127 {
+#[inline(always)]
+pub fn mul_00(_: Gf2p127) -> Gf2p127 {
     return zero();
 }
 
-fn mul_01(a: Gf2p127) -> Gf2p127 {
+#[inline(always)]
+pub fn mul_01(a: Gf2p127) -> Gf2p127 {
     return a;
 }
 
-fn mul_10(a: Gf2p127) -> Gf2p127 {
+#[inline(always)]
+pub fn mul_10(a: Gf2p127) -> Gf2p127 {
     unsafe {
         let sl = slli_epi64(a, 1);
-        let one = srli_epi64(alignr_epi8(a, sl, 8), 63);
-        return sl;
+        let one = srli_epi64(Gf2p127(a.1, sl.0), 63);
+        let lo = Gf2p127(one.1, one.1);
+        let x127x63 = slli_epi64(lo, 63);
+        return add(add(sl, one), x127x63);
     }
+}
+
+#[test]
+fn mul_10_test() {
+  assert_eq!(hex(mul_10(Gf2p127(0, 0))), "00000000000000000000000000000000");
+  assert_eq!(hex(mul_10(Gf2p127(0, 1))), "00000000000000000000000000000002");
+  assert_eq!(hex(mul_10(Gf2p127(0, 1 << 63))), "00000000000000010000000000000000");
+  assert_eq!(hex(mul_10(Gf2p127(1 << 62, 0))), "00000000000000008000000000000001");
+  assert_eq!(hex(mul_10(Gf2p127(1 << 62, 1 << 63))), "00000000000000018000000000000001");
+}
+
+#[inline(always)]
+pub fn mul_11(a: Gf2p127) -> Gf2p127 {
+    return add(mul_01(a), mul_10(a));
+}
+
+pub fn hex(a: Gf2p127) -> String {
+  return std::fmt::format(format_args!("{:016x}{:016x}", a.0, a.1));
+}
+
+#[test]
+fn hex_test() {
+  assert_eq!(hex(zero()), "00000000000000000000000000000000");
+  assert_eq!(hex(from_i64(1)), "00000000000000000000000000000001");
 }
 
 extern {
     #[link_name = "llvm.x86.sse2.pslli.q"]
-    fn slli_epi64(a: simdty::i64x2, b: i32) -> simdty::i64x2;
+    fn slli_epi64(a: Gf2p127, b: i32) -> Gf2p127;
     #[link_name = "llvm.x86.sse2.psrli.q"]
-    fn srli_epi64(a: simdty::i64x2, b: i32) -> simdty::i64x2;
-    #[link_name = "llvm.x86.ssse3.palign.r.128"]
-    fn alignr_epi8(a: simdty::i64x2, b: simdty::i64x2, c: i32) -> simdty::i64x2;
+    fn srli_epi64(a: Gf2p127, b: i32) -> Gf2p127;
 }
